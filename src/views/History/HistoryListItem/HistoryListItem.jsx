199939d065db876edba538065d9a35d17fac15d7
framista <adajab1@gmail.com>
2021-10-03 22:38:58 +0200

    Redirect to details from history

--- src/views/History/HistoryListItem/HistoryListItem.jsx
+++ src/views/History/HistoryListItem/HistoryListItem.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { TouchableOpacity, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useDispatch } from 'react-redux';
+
+import { showPlantDetails } from '../../../store/plantsCurrent/plantsCurrent.actions';
+import { colors } from '../../../constants/layout/colors';
 
 import getStyles from './HistoryListItem.styles';
-import { colors } from '../../../constants/layout/colors';
 
 const HistoryListItem = ({ item }) => {
   const styles = getStyles();
+  const dispatch = useDispatch();
+
+  const openDetails = () => {
+    dispatch(showPlantDetails(item.gbif));
+  };
 
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={openDetails}>
       <Text style={styles.text}>{item.scientificName}</Text>
       <Icon name="read-more" size={40} color={colors.GREEN} />
     </TouchableOpacity>
