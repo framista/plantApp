087df4d277f164dc9977c986f69eaff51dcc8a0a
framista <adajab1@gmail.com>
2021-10-24 20:27:51 +0200

    Remove not used reducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 
 import plantsCurrent from './plantsCurrent/plantsCurrent.reducer';
-import plantsHistory from './plantsHistory/plantsHistory.reducer';
 import iucnStatus from './iucnStatus/iucnStatus.reducer';
 import uiState from './uiState/uiState.reducer';
 
 const rootReducer = combineReducers({
   plantsCurrent,
-  plantsHistory,
   iucnStatus,
   uiState,
 });
